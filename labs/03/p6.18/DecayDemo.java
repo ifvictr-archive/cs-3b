0f1811169cf230164e458d2a7c816fc132b26fb3
Victor Truong <victor@victortruong.com>
2020-06-27 21:20:26 -0700

    Rename to demo, add testing class

---
+++ labs/03/p6.18/DecayDemo.java
@@ -0,0 +1,37 @@
+/*
+ * Lab 3: P6.18 by Victor Truong
+ *
+ * This program tests a mock implementation of a microwave in code and outputs results like a real
+ * microwave would.
+ */
+
+/**
+ * Tests our calculateDecay method.
+ */
+public class DecayDemo {
+    public static void main(String[] args) {
+        final double INITIAL_AMOUNT = 3000;
+        final int HALF_LIFE = 6;
+
+        // Print the amount of dosage remaining for the first 24 hours
+        System.out.println(String.format("Initial Technetium-99 dosage: %.2f", INITIAL_AMOUNT));
+        for (int hour = 1; hour <= 24; hour++) {
+            double currentAmount = DecayDemo.calculateDecay(INITIAL_AMOUNT, hour, HALF_LIFE);
+            System.out.println(
+                    String.format("Amount remaining after %d hours: %.2f", hour, currentAmount));
+        }
+    }
+
+    /**
+     * Calculate the relative amount of material remaining.
+     *
+     * @param initialAmount Initial amount of the material
+     * @param time          Time after the initial amount, in hours
+     * @param halfLife      Half life of the material
+     * @return the remaining material after the specified time
+     */
+    public static double calculateDecay(double initialAmount, double time, double halfLife) {
+        double euler = Math.exp(-time * Math.log(2) / halfLife);
+        return initialAmount * euler;
+    }
+}
--- labs/03/p6.18/DecayTester.java
+++ labs/03/p6.18/DecayTester.java
@@ -1,37 +1,19 @@
-/*
- * Lab 3: P6.18 by Victor Truong
- *
- * This program tests a mock implementation of a microwave in code and outputs results like a real
- * microwave would.
- */
-
 /**
  * Tests our calculateDecay method.
  */
 public class DecayTester {
     public static void main(String[] args) {
-        final double INITIAL_AMOUNT = 3000;
-        final int HALF_LIFE = 6;
-
-        // Print the amount of dosage remaining for the first 24 hours
-        System.out.println(String.format("Initial Technetium-99 dosage: %.2f", INITIAL_AMOUNT));
-        for (int hour = 1; hour <= 24; hour++) {
-            double currentAmount = DecayTester.calculateDecay(INITIAL_AMOUNT, hour, HALF_LIFE);
-            System.out.println(
-                    String.format("Amount remaining after %d hours: %.2f", hour, currentAmount));
-        }
+        test(3000, 0, 6, 3000);
+        test(3000, 6, 6, 1500);
+        test(3000, 12, 6, 750);
+        test(3000, 18, 6, 375);
+        test(3000, 24, 6, 187.5);
     }
 
-    /**
-     * Calculate the relative amount of material remaining.
-     *
-     * @param initialAmount Initial amount of the material
-     * @param time          Time after the initial amount, in hours
-     * @param halfLife      Half life of the material
-     * @return the remaining material after the specified time
-     */
-    public static double calculateDecay(double initialAmount, double time, double halfLife) {
-        double euler = Math.exp(-time * Math.log(2) / halfLife);
-        return initialAmount * euler;
+    public static void test(double initialAmount, double time, double halfLife,
+            double expectedResult) {
+        double result = DecayDemo.calculateDecay(initialAmount, time, halfLife);
+        System.out.println(String.format("Amount %.2f should be %.2f after %d hours, got %.2f",
+                initialAmount, expectedResult, (int) time, result));
     }
 }
